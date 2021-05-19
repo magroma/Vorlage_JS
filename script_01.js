133778da13d5a0a3c4ad033d621d7df8c36d6301
Marco <marco_2@gmx.de>
2021-05-19 09:38:24 +0200

    Vergleiche, größer, kleiner, gleich

--- script_01.js
+++ script_01.js
@@ -1,1 +1,23 @@
 // Entscheidungsstrukturen | control structures
+
+
+// Deklaration
+let ageJohn,ageMark;
+let isJohnOlder,isJohnEqual,isJohnYounger;
+
+//Wertzuweisung
+ageJohn = 25;
+ageMark = 30;
+
+// Logische Aussagen / Test(s)
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+isJohnYounger = (ageJohn < ageMark);
+
+// Ausgabezweig
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("isJohnYounger: " + isJohnYounger);
+
+/************ IF ************/
+// alternativlos (TINA There is no alternative)
